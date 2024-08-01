fd8c739792c0af56ff94bda359f0a3a18c963b92
jhon1253 <sebastianquinayas@gmail.com>
2024-07-31 22:43:31 -0500

    custom

---
+++ custom_array/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { Route } from "react-router-dom";
+import About from "./pages/About";
+import CategoryProducts from "./pages/CategoryProducts";
+import NotificationMessage from "./Componets/NotificationMessage";
+import Categories from "./pages/Categories";
+import Navbar from "./Componets/Navbar";
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/categories" element={<Categories />}></Route>
+          <Route path="/categoryproducts" element={<CategoryProducts />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
