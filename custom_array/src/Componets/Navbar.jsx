f50367a3da36e3102348d14a84ac6305caf5fdca
jhon1253 <sebastianquinayas@gmail.com>
2024-08-01 11:43:56 -0500

    custom

--- custom_array/src/Componets/Navbar.jsx
+++ custom_array/src/Componets/Navbar.jsx
@@ -1,4 +1,3 @@
-// Navbar.js
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -14,7 +13,7 @@ const Navbar = () => {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button */}
+            {/*menu button */}
             <button
               type="button"
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
