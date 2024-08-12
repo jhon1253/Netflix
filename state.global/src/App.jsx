b713bfd29be0c18cb4ac24710670cc69519d6949
jhon1253 <sebastianquinayas@gmail.com>
2024-08-12 08:55:04 -0500

    cam-state.global

--- state.global/src/App.jsx
+++ state.global/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ProductList from './Components/ProductsList'
-import CartIcon from './Components/CartIcons/CartIcon'; // Asegúrate de que la ruta sea correcta
+import CartIcon from './Components/CartIcons/CartIcon';
 
 function App() {
   return (
     <div>
       <CartIcon /> {/* Agrega el ícono del carrito */}
-      <div>app</div>
+      <div>Cart</div>
       <ProductList />
     </div>
   );
